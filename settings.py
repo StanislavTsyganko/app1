f1a483fe66871fc22eec39ade79e8a5d15ec9669
StanislavTsyganko <sts035@mail.ru>
2025-07-30 12:46:53 +0300

    add main page

--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'integration_utils.bitrix24',
 ]
 
 MIDDLEWARE = [
---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+
+@main_auth(on_cookies=True)
+def main_page(request):
+    user = request.bitrix_user_token
+
+    return render(request, 'main_page.html', context={'user': user})
