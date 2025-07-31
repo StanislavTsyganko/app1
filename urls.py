179d438c6280f0de210c079ba4445d8920d7170b
StanislavTsyganko <sts035@mail.ru>
2025-07-31 13:20:33 +0300

    add some style

--- urls.py
+++ urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 import views
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('deals/',include('deals.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
