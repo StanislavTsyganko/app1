0fe93d102268965ad8223762be7708b5aa43aa35
StanislavTsyganko <sts035@mail.ru>
2025-07-31 11:07:50 +0300

    add table of deals

--- deals/views.py
+++ deals/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+from django.http import JsonResponse
+from views import get_name
 
+@main_auth(on_cookies=True)
 def list(request):
-    return
+    try:
+        bx = request.bitrix_user_token
+        deals = bx.call_list_method('crm.deal.list', {
+            'filter': {'ASSIGNED_BY_ID': bx.user_id, 'STAGE_ID': 'PREPARATION'},
+            'select': ['ID', 'TITLE', 'STAGE_ID', 'OPPORTUNITY', 'DATE_CREATE'],
+            'order': {'DATE_CREATE': 'DESC'},
+            'start': 0
+        })[:10]
+
+        full_name = get_name(request)
+
+        return render(request, 'list.html', {
+            'user': full_name,
+            'deals': deals,
+            'user_id': bx.user_id
+        })
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 
 def add(request):
--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.shortcuts import redirect
@@ -12,19 +11,25 @@ def main_page(request):
     try:
         if not hasattr(request, 'bitrix_user_token'):
             return
-        user = request.bitrix_user_token
-        print(user)
-        user_data = user.call_api_method('user.get', {
-            'ID': user.user_id
-        })
-        result = user_data.get('result', [{}])[0]
-        first_name = result.get('NAME', '')
-        last_name = result.get('LAST_NAME', '')
 
-        full_name = f"{last_name} {first_name}".strip()
+        full_name = get_name(request)
 
         return render(request, 'main_page.html', {
-            'user': full_name if full_name else 'Пользователь'
+            'user': full_name
         })
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
+
+
+def get_name(request):
+    user = request.bitrix_user_token
+    user_data = user.call_api_method('user.get', {
+        'ID': user.user_id
+    })
+    result = user_data.get('result', [{}])[0]
+    first_name = result.get('NAME', '')
+    last_name = result.get('LAST_NAME', '')
+
+    full_name = f"{last_name} {first_name}".strip()
+
+    return full_name if full_name else 'Пользователь'
